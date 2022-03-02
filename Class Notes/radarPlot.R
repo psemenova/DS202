aed48a5afb3099350f56850f1ef678904949b1d0
psemenova <psemenova0201@gmail.com>
2022-03-02 12:52:23 -0600

    adding notes from 1/2/22

--- Class Notes/radarPlot.R
+++ Class Notes/radarPlot.R
@@ -99,7 +99,7 @@ fbi1 = fbi1 %>% group_by(Type) %>% mutate(
 fbi %>% filter(best == 1) %>% select(Type, State, Year, Rate)
 
 
-# how many times the states apperared in the top for crime 
+# how many times the states appeared in the top for crime 
 fbi1 = fbi1 %>% group_by(Type, Year) %>% mutate(
   best = rank(Rate) # ranks from lowest rate to highest rate
 )
@@ -108,3 +108,50 @@ fbi2 %>% ggplot(aes(x = State)) +
   geom_bar() + 
   facet_wrap(~Type, scales = 'free') +
   coord_flip()
+
+
+# French Fries Data
+data(french_fries, package="reshape2")
+french_fries.mean = 
+french_fries %>% group_by(time) %>% summarise(
+  m.potato = mean(potato, na.rm=TRUE),
+  m.buttery = mean(buttery, na.rm=TRUE),
+  m.grassy = mean(grassy, na.rm=TRUE),
+  m.rancid = mean(rancid, na.rm=TRUE),
+  m.painty = mean(painty, na.rm=TRUE)
+) 
+
+
+french_fries.mean %>% mutate(time = as.numeric(as.character(time))) %>%
+  ggplot(aes(x = time)) + 
+  geom_point(aes(y = m.potato)) +
+  geom_point(shape=2, aes(y=m.rancid)) + 
+  geom_point(shape=3, aes(y=m.painty)) +
+  geom_line(aes(x=time, y=m.potato)) +
+  geom_line(linetype = 2, aes(x=time, y=m.rancid)) +
+  geom_line(linetype = 3, aes(x=time, y=m.painty)) +
+  ylab("Average")
+
+# do ratings of potato-y show a difference between the different oils over time?
+# draw a plot of the average rating by time, color by treatment. 
+# how does this plot look like for the rancid rating?
+french_fries.trt.mean = 
+  french_fries %>% group_by(time, treatment) %>% summarise(
+    m.potato = mean(potato, na.rm=TRUE),
+    m.buttery = mean(buttery, na.rm=TRUE),
+    m.grassy = mean(grassy, na.rm=TRUE),
+    m.rancid = mean(rancid, na.rm=TRUE),
+    m.painty = mean(painty, na.rm=TRUE)
+  )
+
+french_fries.trt.mean %>% mutate(time = as.numeric(as.character(time))) %>% 
+  ggplot(aes(x = time, col = treatment)) +
+  geom_point(aes(y = m.potato)) +
+  geom_point(shape=2, aes(y=m.rancid)) + 
+  geom_point(shape=3, aes(y=m.painty)) +
+  geom_line(aes(x=time, y=m.potato)) +
+  geom_line(linetype = 2, aes(x=time, y=m.rancid)) +
+  geom_line(linetype = 3, aes(x=time, y=m.painty)) +
+  ylab("Average")
+
+
