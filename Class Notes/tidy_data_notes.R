8c3e78e870e6d6bddbfe59ea55106c4d8182b85f
psemenova <psemenova0201@gmail.com>
2022-03-07 16:30:49 -0600

    adding Michael's contribution to HW 6

---
+++ Class Notes/tidy_data_notes.R
@@ -0,0 +1,8 @@
+vignette("tidy-data", package="tidyr")
+
+# Gather - take multiple columns and collapse into key-value pairs 
+
+# Spread - a key-value pair across multiple columns
+
+# Separate - one column into multiple columns 
+
