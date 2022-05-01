178d50d8c37f7c6195842d0ce8bffa972ad32541
psemenova <psemenova0201@gmail.com>
2022-05-01 12:54:09 -0500

    adding hw 11

--- Class Notes/polishing.R
+++ Class Notes/polishing.R
@@ -1,5 +1,5 @@
 library(tidyverse)
-# color scales 
+# COLOR SCALES
 
 # default continuous color scheme
 # scale_color_gradient -> used for continuous 
@@ -13,13 +13,25 @@ p2 <- mpg %>% filter(year == 2008) %>%
   ggplot(aes(x = cty, y = hwy, colour = factor(cyl))) +
   geom_point()
 p2 + scale_color_discrete()
+p2 + scale_color_brewer(palette = "Set1")
+p2 + scale_color_brewer(palette = "Dark2")
+p2 + scale_color_brewer(palette = "Greens")
+p2 = p2 +  scale_color_brewer(palette = "Set1")
+p2
+p2 = p2 + xlab("city miles per gallon") + ylab("highway miles per gallon") + ggtitle("city vs. highway")
+p2 = p2 + theme(plot.title = element_text(hjust = 0.5)) + theme(plot.title = element_text(size = 22)) # center the title
+p2 = p2 + labs(color = "number of cyl") # change name of legend
+p2 = p2 + theme(legend.position = "bottom") # move the legend to the bottom of the plot
+p2
 
 
+# THEMES 
 library(RColorBrewer)
 display.brewer.all()
 
 p <- mpg %>% ggplot(aes(x = displ, y =  cty, colour= factor(class))) + geom_point()
-p + theme_grey()
+p + theme_grey() #default -> grey background/white grid
+p + theme_bw() # white background/black grid
 
 
 p <- mtcars %>% ggplot(aes(x = wt, y =  mpg, colour= factor(cyl))) + geom_point()
@@ -30,3 +42,11 @@ p + theme_dark()
 library(ggthemes)
 p + theme_excel() 
 p + theme_fivethirtyeight()
+
+
+# element_line/text/blank
+
+# CHANGING ELEMENTS MANUALLY
+mpg %>% ggplot(aes(x = manufacturer)) + geom_bar() +
+  theme(axis.text.x = element_text(angle=45, vjust=1, hjust=1))
+
--- Class Notes/web_scraping.R
+++ Class Notes/web_scraping.R
@@ -1,1 +1,20 @@
 library(rvest)
+library(tidyverse)
+
+url <- "https://www.the-numbers.com/weekend-box-office-chart"
+html <- read_html(url) # 'read_html' gets all the information from a URL
+html
+
+tables <- html %>% html_table(fill=TRUE) # extracts all tables from the sources html inta a list of data frames
+length(tables)
+tables[[1]]
+A = tables[[2]] # get the second data frame in tables 
+names(A)
+names(A)[1:2] <- c("Rank", "Rank.Last.Week")
+str(A)
+
+A = A %>% mutate(TotalGross = parse_number(TotalGross),
+                 PerTheater = parse_number(PerTheater),
+                 Theaters = parse_number(Theaters),
+                 Gross = parse_number(Gross))
+
